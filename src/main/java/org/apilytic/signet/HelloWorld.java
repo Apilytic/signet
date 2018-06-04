64f03e8cf7e2ed21a64ac1e470bb4803f2f622c8
Georgi Lambov <georgi.lambov@gmail.com>
2018-06-04 23:49:47 +0300

    Format.

--- src/main/java/org/apilytic/signet/HelloWorld.java
+++ src/main/java/org/apilytic/signet/HelloWorld.java
@@ -3,9 +3,8 @@ package org.apilytic.signet;
 import io.reactivex.Flowable;
 
 public class HelloWorld {
+
 	public static void main(String[] args) {
-
 		Flowable.just("Hell World").subscribe(System.out::println);
-
 	}
 }
