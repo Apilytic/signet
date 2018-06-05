ff36a466df640cf02bb5e0791e8de42b6eef3179
Georgi Lambov <georgi.lambov@gmail.com>
2018-06-06 00:24:10 +0300

    Organize import.

--- src/main/java/org/apilytic/signet/DiscardServerHandler.java
+++ src/main/java/org/apilytic/signet/DiscardServerHandler.java
@@ -1,11 +1,11 @@
 package org.apilytic.signet;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
-@ChannelHandler.Sharable
+@Sharable
 public class DiscardServerHandler extends SimpleChannelInboundHandler<Object> {
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
