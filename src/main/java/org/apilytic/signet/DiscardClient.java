9d89f94c615566c13ab384c9ad1f04471c307f5b
Georgi Lambov <georgi.lambov@gmail.com>
2018-06-11 23:47:05 +0300

    Default port to 8080.

--- src/main/java/org/apilytic/signet/DiscardClient.java
+++ src/main/java/org/apilytic/signet/DiscardClient.java
@@ -15,7 +15,7 @@ import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 public class DiscardClient {
 	static final boolean SSL = System.getProperty("ssl") != null;
 	static final String HOST = System.getProperty("host", "127.0.0.1");
-	static final int PORT = Integer.parseInt(System.getProperty("port", "8009"));
+	static final int PORT = Integer.parseInt(System.getProperty("port", "8080"));
 	static final int SIZE = Integer.parseInt(System.getProperty("size", "256"));
 
 	public static void main(String[] args) throws Exception {
