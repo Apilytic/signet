82c1e1b74d9f31cc1dd8edb0322a19df160ee91d
Georgi Lambov <georgi.lambov@gmail.com>
2018-06-12 23:52:29 +0300

    Handlers wraps input and returns json to client.

--- src/main/java/org/apilytic/signet/EchoServerHandler.java
+++ src/main/java/org/apilytic/signet/EchoServerHandler.java
@@ -1,25 +1,35 @@
 package org.apilytic.signet;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
 
 @Sharable
 public class EchoServerHandler extends ChannelInboundHandlerAdapter {
 
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) {
-		ctx.write(msg);
+		ByteBuf in = (ByteBuf) msg;
+		System.out.println(
+				"Server received: " + in.toString(CharsetUtil.UTF_8));
+
+		String wrapIn = in.toString(CharsetUtil.UTF_8);
+		String json = "{\"userId\": \"" + wrapIn + "\"}";
+
+		ctx.write(Unpooled.copiedBuffer(json, CharsetUtil.UTF_8));
 	}
 
 	@Override
 	public void channelReadComplete(ChannelHandlerContext ctx) {
-		ctx.flush();
+		ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
 	}
 
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-		// Close the connection when an exception is raised.
 		cause.printStackTrace();
 		ctx.close();
 	}
